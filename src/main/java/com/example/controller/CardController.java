626efa4e08770e2688f6ef50d8546b6d32272535
vineet1288 <12vineet@gmail.com>
2025-05-13 18:21:28 +0530

    Kafka Producer and Consumer with 3 partition

--- src/main/java/com/example/controller/CardController.java
+++ src/main/java/com/example/controller/CardController.java
@@ -1,8 +1,10 @@
 package com.example.controller;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.example.entity.Card;
+import com.example.repository.CardRepository;
+import com.example.validation.CardValidation;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.entity.Card;
-import com.example.repository.CardRepository;
-import com.example.validation.CardValidation;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CardController {
@@ -23,13 +24,22 @@ public class CardController {
 	
 	@Autowired
     private CardRepository repository;
-	
+
 	@Autowired
 	CardValidation validation;
+
+	private final Counter myCounter;
+
+	public CardController(MeterRegistry registry) {
+		this.myCounter = Counter.builder("my_card_counter")
+				.description("A Card counter metric")
+				.register(registry);
+	}
 	
 	@PostMapping(path="/add" , consumes = "application/json" , produces = "application/json")
 	public ResponseEntity<Object> addNewCard(@RequestBody Card card){
 		System.out.println(card);
+		myCounter.increment();
 		Optional<String> validate = this.validation.validateCard(card);
 		if(!validate.isEmpty()) {
 			logger.info("Validation failed for card number", validate.get());
